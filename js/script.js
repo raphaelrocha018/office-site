357fc47f41fad01756b77ea94407e1ae1cf5a2f6
Raphael Rocha <raphaelrocha18@outlook.com>
2023-03-15 22:23:03 -0300

    home mobile

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// ANIMAÇÃO SCROLL
+
+function animacaoScroll() {
+    const sections = document.querySelectorAll(".js-scroll");
+    const metadeTela = window.innerHeight * 0.5;
+
+    if (sections.length) {
+        function animarSections(event) {
+            sections.forEach((section) => {
+                const distanciaTopo =
+                    section.getBoundingClientRect().top - metadeTela;
+                if (distanciaTopo < 0) {
+                    section.classList.add("animar");
+                }
+            });
+        }
+    }
+    animarSections();
+    window.addEventListener("scroll", animarSections);
+}
+
+animacaoScroll();
