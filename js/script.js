77c714e8a45550b2b1cf4a36bbf3d4c3ef9e423b
Raphael Rocha <raphaelrocha18@outlook.com>
2023-03-15 22:27:52 -0300

    sem js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // ANIMAÇÃO SCROLL
 
-function animacaoScroll() {
-    const sections = document.querySelectorAll(".js-scroll");
-    const metadeTela = window.innerHeight * 0.5;
+// function animacaoScroll() {
+//     const sections = document.querySelectorAll(".js-scroll");
+//     const metadeTela = window.innerHeight * 0.5;
 
-    if (sections.length) {
-        function animarSections(event) {
-            sections.forEach((section) => {
-                const distanciaTopo =
-                    section.getBoundingClientRect().top - metadeTela;
-                if (distanciaTopo < 0) {
-                    section.classList.add("animar");
-                }
-            });
-        }
-    }
-    animarSections();
-    window.addEventListener("scroll", animarSections);
-}
+//     if (sections.length) {
+//         function animarSections(event) {
+//             sections.forEach((section) => {
+//                 const distanciaTopo =
+//                     section.getBoundingClientRect().top - metadeTela;
+//                 if (distanciaTopo < 0) {
+//                     section.classList.add("animar");
+//                 }
+//             });
+//         }
+//     }
+//     animarSections();
+//     window.addEventListener("scroll", animarSections);
+// }
 
-animacaoScroll();
+// animacaoScroll();
